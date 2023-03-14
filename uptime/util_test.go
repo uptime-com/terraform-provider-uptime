338e576f08cdadb834259263904ef8c000906898
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2023-03-14 09:04:50 +0100

    Extend error accumulation test suite

--- uptime/util_test.go
+++ uptime/util_test.go
@@ -9,20 +9,46 @@ import (
 )
 
 func TestAccumulateErrors(t *testing.T) {
-	errs := []error{
-		nil,
-		nil,
-		errors.New("foo"),
-		nil,
-		errors.New("bar"),
-	}
+	t.Run("all errors", func(t *testing.T) {
+		errs := []error{
+			errors.New("foo"),
+			errors.New("bar"),
+		}
 
-	err := accumulateErrors(errs...)
-	require.Error(t, err)
+		err := accumulateErrors(errs...)
+		require.Error(t, err)
 
-	merr := new(multierror.Error)
-	if !errors.As(err, &merr) {
-		t.Fatal("err is not a multierror")
-	}
-	require.Len(t, merr.Errors, 2)
+		merr := new(multierror.Error)
+		if !errors.As(err, &merr) {
+			t.Fatal("err is not a multierror")
+		}
+		require.Len(t, merr.Errors, 2)
+	})
+	t.Run("some errors", func(t *testing.T) {
+		errs := []error{
+			nil,
+			nil,
+			errors.New("foo"),
+			nil,
+			errors.New("bar"),
+		}
+
+		err := accumulateErrors(errs...)
+		require.Error(t, err)
+
+		merr := new(multierror.Error)
+		if !errors.As(err, &merr) {
+			t.Fatal("err is not a multierror")
+		}
+		require.Len(t, merr.Errors, 2)
+	})
+	t.Run("no errors", func(t *testing.T) {
+		errs := []error{
+			nil,
+			nil,
+			nil,
+		}
+		err := accumulateErrors(errs...)
+		require.NoError(t, err)
+	})
 }
