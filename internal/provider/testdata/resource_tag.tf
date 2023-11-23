fc49b63c200fa8537cd2230c480d88655fd42b27
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2023-11-23 18:28:05 +0100

    Cleanup redundant stuff from testdata

--- internal/provider/testdata/resource_tag.tf
+++
@@ -1,9 +0,0 @@
-resource uptime_tag test {
-  tag       = "{{ petname 3 "-" }}"
-  color_hex = "#ff0000"
-}
-// ---
-resource uptime_tag test {
-  tag       = "{{ petname 3 "-" }}"
-  color_hex = "#00ff00"
-}
