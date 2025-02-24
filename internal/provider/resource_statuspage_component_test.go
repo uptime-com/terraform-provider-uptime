22bd1e4f5492d1b76e27915a8c043f8a74345e0f
Givi Khojanashvili <gigovich@gmail.com>
2025-02-24 16:17:04 +0400

    fix: SYS-569 clean status page component service_id and group_id (#139)

--- internal/provider/resource_statuspage_component_test.go
+++ internal/provider/resource_statuspage_component_test.go
@@ -43,3 +43,42 @@ func TestAccStatusPageComponentResource(t *testing.T) {
 		},
 	})
 }
+
+func TestAccStatusPageComponentResource_Clean(t *testing.T) {
+	name := petname.Generate(3, "-")
+	componentNames := [2]string{
+		petname.Generate(3, "-"),
+		petname.Generate(3, "-"),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { _ = testAccAPIClient(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/resource_statuspage_component/clean_step1"),
+				ConfigVariables: config.Variables{
+					"name":           config.StringVariable(name),
+					"component_name": config.StringVariable(componentNames[0]),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("uptime_statuspage.test", "name", name),
+					resource.TestCheckResourceAttr("uptime_statuspage_component.test", "name", componentNames[0]),
+				),
+			},
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/resource_statuspage_component/clean_step2"),
+				ConfigVariables: config.Variables{
+					"name":           config.StringVariable(name),
+					"component_name": config.StringVariable(componentNames[1]),
+				},
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("uptime_statuspage_component.test", "name", componentNames[1]),
+					resource.TestCheckResourceAttr("uptime_statuspage_component.test", "description", ""),
+					resource.TestCheckNoResourceAttr("uptime_statuspage_component.test", "group_id"),
+					resource.TestCheckNoResourceAttr("uptime_statuspage_component.test", "service_id"),
+				),
+			},
+		},
+	})
+}
